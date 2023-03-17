a3b49d6cd8c538a7228ac2618a39757243621126
unknown <mannmanjeet988@gmail.com>
2023-03-17 19:22:03 +0530

    Eancapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+
+       // obj.name = "Manjeet";    Name can't be set like this as name variable is private
+
+        obj.setName("Manjeet");
+        String ans = obj.getName();
+        System.out.println(ans);
+    }
 }
